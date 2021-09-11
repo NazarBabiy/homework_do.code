9c8a33b82908f16fed1026278f8dc79b04e5e69a
NazarBabiy <babiynp@gmail.com>
2021-09-11 21:42:08 +0300

    fix_1

--- lesson_3/script.js
+++ lesson_3/script.js
@@ -8,23 +8,21 @@ function arr_change() {
     const arr_sum = [...arr1,...arr2];
 
     // Перевіряє на унікальність елементів
-    let arr = Array.from(new Set(arr_sum))
+    let arr = Array.from(new Set(arr_sum));
 
-    let result = ''
-    let result_1 = ''
+    let result = '';
     let result_0 =[]
 
     // Перетворює елементи: всі букви великі значення та пишуться заду на перед
     for (let i = 0; i<arr.length; i++){
         result = String(arr[i])
-        result = result.split("").reverse().join("")
-        result_1 = result.toUpperCase()
+        result = result.split("").reverse().join("").toUpperCase()
     
-        result_0.push(result_1)
+        result_0.push(result)
     }
 
     // Останнє переворення з Array у String та сполученням
-     return  finish = result_0.join('-')
+     return  finish = result_0.join(' - ')
 
 }
 
